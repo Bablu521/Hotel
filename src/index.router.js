a755181075bce8b71480a4bfbc87f65faaf2d955
Mohamed Hossam El-Bably <146677060+Bablu521@users.noreply.github.com>
2024-10-11 18:11:21 +0300

    errorHandling

--- src/index.router.js
+++ src/index.router.js
@@ -1,4 +1,5 @@
 import connectDB from './connection.js';
+import { globalErrorHandling } from './utils/errorHandling.js';
 
 const initApp = (app, express) => {
     app.use(express.json())
@@ -7,5 +8,6 @@ const initApp = (app, express) => {
     app.use("*", (req, res, next) => {
         return res.status(404).json({ message: "IN-VALID ROUTING" })
     })
+    app.use(globalErrorHandling)
 }
 export default initApp
